a2d9b871db320f21d3698e0992ab59c620845535
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-10-07 11:31:19 +0530

    Update app.js

--- tool/app.js
+++ tool/app.js
@@ -7,36 +7,6 @@ const PORT = process.env.PORT || 6000;
 app.use(express.json());
 
 
-
-
-app.get('/', async (req, res) => {
-    try {
-        console.log("Pipeline\n", pipeline);
-        const translator = await pipeline('translation', 'Xenova/nllb-200-distilled-600M');
-        // const input_data = await req.body.data;
-        const translated_text = await translator("Please translate this text.", {
-            src_lang: 'eng_Latn',
-            tgt_lang: 'hin_Deva'
-        });
-        const responseData = translated_text;
-
-        res.status(200).json({
-            msg: "Translation done successfully.",
-            data: responseData
-        })
-
-
-    } catch (error) {
-        res.status(500).json({
-            fullError: error.stack,
-            msg: error.message,
-            extra: "Functional flaw."
-        })
-    }
-
-})
-
-
 app.post('/', async (req, res) => {
     const input_data = await req.body.data;
     try {
@@ -46,11 +16,10 @@ app.post('/', async (req, res) => {
             src_lang: 'eng_Latn',
             tgt_lang: 'hin_Deva'
         });
-        const responseData = translated_text;
 
         res.status(200).json({
             msg: "Translation done successfully.",
-            data: responseData
+            data: translated_text[0].translation_text
         })
 
 
